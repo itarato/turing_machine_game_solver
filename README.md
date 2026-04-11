d11acd29f813334909ce08e049350def6eec2abb
Peter Arato <it.arato@gmail.com>
2026-04-11 06:52:01 -0400

    Add download cache.

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 Game: https://www.turingmachine.info/
 
 ![Screenshot](misc/screenshot.png)
+
+## Use
+
+```bash
+# Pre-download problems:
+> python lib/download.py <FROM:int> <TO:int>
+
+# Run game:
+> python main.py <ID:int>
+```
--- lib/downloader.py
+++ lib/downloader.py
@@ -1,6 +1,10 @@
 import requests
 import time
 import json
+import pickle
+import sys
+
+CACHE_FILE_PATH = "./cache/problems.pkl"
 
 
 class Problem:
@@ -10,37 +14,62 @@ class Problem:
         self.secret = secret
 
 
-def load_problems(rng: range):
-    for i in rng:
-        problem = load_problem(i)
-        print(problem)
-        time.sleep(1)
+class Downloader:
+    def __init__(self):
+        try:
+            self.cache = pickle.load(open(CACHE_FILE_PATH, "rb"))
+        except:
+            self.cache = {}
 
+    def load_problems(self, rng: range):
+        for i in rng:
+            problem = self.load_problem(i)
+            print(problem)
 
-def load_problem(id: int) -> Problem:
-    url = "https://turingmachine.info/api/api.php"
-    params = {"uuid": "6d4918c2-ab2b-408d-a186-6308deb6f144", "h": str(id)}
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:149.0) Gecko/20100101 Firefox/149.0",
-        "Accept": "*/*",
-        "Accept-Language": "en-CA,en-US;q=0.9,en;q=0.8",
-        "Accept-Encoding": "gzip, deflate, br, zstd",
-        "Referer": "https://www.turingmachine.info/",
-        "Origin": "https://www.turingmachine.info",
-        "Connection": "keep-alive",
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "same-site",
-        "Priority": "u=4",
-    }
+    def load_problem(self, id: int) -> Problem:
+        if id in self.cache:
+            parsed = self.cache[id]
+        else:
+            parsed = self.load_source(id)
+            time.sleep(1)
 
-    response = requests.get(url, params=params, headers=headers)
+        return Problem(id, parsed["ind"], parsed["code"])
 
-    assert response.status_code == 200
-    parsed = json.loads(response.text)
+    def load_source(self, id: int) -> dict:
+        url = "https://turingmachine.info/api/api.php"
+        params = {"uuid": "6d4918c2-ab2b-408d-a186-6308deb6f144", "h": str(id)}
+        headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:149.0) Gecko/20100101 Firefox/149.0",
+            "Accept": "*/*",
+            "Accept-Language": "en-CA,en-US;q=0.9,en;q=0.8",
+            "Accept-Encoding": "gzip, deflate, br, zstd",
+            "Referer": "https://www.turingmachine.info/",
+            "Origin": "https://www.turingmachine.info",
+            "Connection": "keep-alive",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-site",
+            "Priority": "u=4",
+        }
 
-    return Problem(id, parsed["ind"], parsed["code"])
+        response = requests.get(url, params=params, headers=headers)
+
+        assert response.status_code == 200
+        parsed = json.loads(response.text)
+
+        self.cache[id] = parsed
+        pickle.dump(self.cache, open(CACHE_FILE_PATH, "wb"))
+
+        return parsed
 
 
 if __name__ == "__main__":
-    load_problems(range(1, 2))
+    if len(sys.argv) == 3:
+        start = int(sys.argv[1])
+        end = int(sys.argv[2])
+    else:
+        start = 1
+        end = 1
+
+    dl = Downloader()
+    dl.load_problems(range(start, end + 1))
